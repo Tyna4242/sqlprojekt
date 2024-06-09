8b4bb562b48ef34d4dae1769df14ba5c8e09f186
Tyna4242 <150627849+Tyna4242@users.noreply.github.com>
2024-06-09 13:56:24 +0200

    Create SELECT.sql

---
+++ SELECT.sql
@@ -0,0 +1,4 @@
+SELECT * FROM trainers_with_highest_student_outreach;
+SELECT * FROM trainers_teaching_atleast_2_courses;
+SELECT * FROM report_all_data;
+SELECT * FROM courses_with_most_signed_up_students;
